610723701c70d40f5f9542e3610b59a10bc1fbc4
Jenkos <you@example.com>
2025-01-03 09:25:38 -0800

    install bootsrap icon and edited the footer component

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,7 +9,7 @@ export default function Footer(){
                     {/* Email Icon */}
                   <a href="mailto:kokospicy23@gmail.com" className="text-white mx-3" title="Email">
                     <img
-                     src=""
+                     src="https://imgs.search.brave.com/FINBnnb4L-rMuWBkQgurn5PbR3Wneg0hYeCHWDYx59c/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAxLzgxLzAwLzM0/LzM2MF9GXzE4MTAw/MzQ5MF9DeFc0ZlEw/SDNWeXBJSXNQa0ZH/cE1EdmlPOHlzV2pP/Wi5qcGc"
                      alt="Email"
                     style={{ width: "30px" }}
                   />
@@ -17,7 +17,7 @@ export default function Footer(){
                   {/* Twitter Icon */}
                  <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-white mx-3" title="Twitter">
                      <img
-                      src=""
+                      src="https://imgs.search.brave.com/1J4MKqARDDWNTxD0kn4y2ePJojU65vRm5e1DBnvvmNY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91eHdp/bmcuY29tL3dwLWNv/bnRlbnQvdGhlbWVz/L3V4d2luZy9kb3du/bG9hZC9icmFuZHMt/YW5kLXNvY2lhbC1t/ZWRpYS94LXNvY2lh/bC1tZWRpYS1yb3Vu/ZC1pY29uLnN2Zw"
                       alt="Twitter"
                       style={{ width: "30px" }}
                 />
@@ -28,6 +28,16 @@ export default function Footer(){
                    <img
                     src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg"
                     alt="Instagram"
+                    style={{ width: "30px" }}
+                />
+                
+               </a>
+
+                {/* tiktok Icon */}
+                <a href="https://www.tiktok.com/en" target="_blank" rel="noopener noreferrer" className="text-white mx-3" title="Instagram">
+                   <img
+                    src="https://imgs.search.brave.com/2kVpgwihLaDyHG8ybtsFjP0Meax8caz3MoKxT_lAE6w/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzA1LzMyLzIwLzYy/LzM2MF9GXzUzMjIw/NjI2NV9KSGUzYWFs/dms5Wk5MZ1JOQXRP/cVFucDBUdGVEV2ty/cS5qcGc"
+                    alt="tiktok"
                     style={{ width: "30px" }}
                 />
                 
