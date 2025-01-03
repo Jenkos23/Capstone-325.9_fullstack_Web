bfbb26d991060472a8140393dff2e9d24efbb1e0
Jenkos <you@example.com>
2025-01-03 08:32:05 -0800

    rearranged the content to render before the image

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -4,7 +4,8 @@ import '../styles/article.css'
 export default function Article({title, images, content}){
     return(
         <article className="article">
-            <h2>{title}</h2>
+            <h2  className="agu-display">{title}</h2>
+            <p className="styled-paragraph">{content}</p>
 
           <div className="image-container">
             {images.map((image, index) =>(
@@ -15,7 +16,7 @@ export default function Article({title, images, content}){
 
            </div>
 
-            <p className="styled-paragraph">{content}</p>
+            
         </article>
     )
 }
